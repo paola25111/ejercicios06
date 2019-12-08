ae6df391e2668722a988adf8bc9a668db1f3e77c
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:04:40 -0500

    se agrego el 5to ejercicio

---
+++ ejercicio05.py
@@ -0,0 +1,13 @@
+#ejercicio 05
+
+import os
+#declaracion
+
+alumno,total_de_creditos="",0
+alumno=os.sys.argv[1]
+total_de_creditos=int(os.sys.argv[2])
+#pressecing
+if (total_de_creditos>24):
+    print(alumno,"puede matricularse")
+
+#fin_if
