95d461d155c61bc2dde81756f90a81b1e3135168
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 17:01:15 -0500

    se agrego el 18vo ejercicio

---
+++ ejercicio18.py
@@ -0,0 +1,17 @@
+#ejercicio 18
+
+import os
+#declaracion
+
+nombre,nro_tickets="",0
+#inputvia os
+nombre=os.sys.argv[1]
+nro_tickets=int(os.sys.argv[2])
+#pressecing
+#si el nro de tickets supera los 50
+#mostrar "ganaste un paneton
+if (nro_tickets>50):
+    print(nombre,"ganaste un paneton")
+else:
+    print(nombre,"sigue intentando")
+#fin_if
