5054076b1f03778c384871ef8ec353dbf7c183ad
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:59:54 -0500

    se agrego el 16vo ejercicio

---
+++ ejercicio16.py
@@ -0,0 +1,15 @@
+#ejercicio 16
+
+import os
+#declaracion
+
+producto,costo="",0.0
+producto=os.sys.argv[1]
+costo=float(os.sys.argv[2])
+#pressecing
+#si el producto menor de 30 soles
+#mostrar producto en liquidacion
+if (costo<30):
+    print(producto,"en liquidacion")
+
+#fin_if
