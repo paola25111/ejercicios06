236eff27ad339938f2128a362072d55e966df573
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:42:48 -0500

    se agrego el 13vo ejercicio

---
+++ ejercicio13.py
@@ -0,0 +1,15 @@
+#ejercicio 13
+
+import os
+#declaracion
+
+cliente,total_compras="",0.0
+cliente=os.sys.argv[1]
+total_compras=float(os.sys.argv[2])
+#pressecing
+if (total_compras>70):
+    print(cliente,"gano canasta")
+else:
+    print(cliente,"para la proxima oportunidad")
+
+#fin_if
