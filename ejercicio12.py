9009b943628242e10ec3971ec727841d76e89b56
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:42:12 -0500

    se agrego el 12avo ejercicio

---
+++ ejercicio12.py
@@ -0,0 +1,15 @@
+#ejercicio 12
+
+import os
+#declaracion
+
+alumno,curso_aprobado="",0
+alumno=os.sys.argv[1]
+curso_aprobado=int(os.sys.argv[2])
+#pressecing
+if (curso_aprobado>=11):
+    print(alumno,"aprobado")
+else:
+    print(alumno,"desaprobado")
+
+#fin_if
