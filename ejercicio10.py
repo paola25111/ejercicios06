db091acc4f374377ea030047d5c42c2fdbb18b7e
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:41:15 -0500

    se agrego el 10mo ejercicio

---
+++ ejercicio10.py
@@ -0,0 +1,13 @@
+#ejercicio 10
+import os
+#declaracion
+
+cliente,costo_entreda="",0.0,
+cliente=os.sys.argv[1]
+costo_entreda=float(os.sys.argv[2])
+#pressecing
+#si el costo es mayor a 40
+#mostrar zona vip
+if (costo_entreda>40):
+    print(cliente,"zona vip")
+#fin_if
