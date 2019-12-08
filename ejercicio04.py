e15a347c11a3eb31b3d5df84042595119da45849
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:04:10 -0500

    se agrego el 4to ejercicio

---
+++ ejercicio04.py
@@ -0,0 +1,12 @@
+#ejercicio 04
+
+import os
+#declaracion
+cliente,numero_ticke="",0
+cliente=os.sys.argv[1]
+numero_ticke=int(os.sys.argv[2])
+#pressecing
+if (numero_ticke>25):
+    print(cliente,"entra al sorteo")
+
+#fin_if
