25ebb973c597405e18630fc43f0e8b78b59d6f9f
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:38:45 -0500

    se agrego el 8vo ejercicio

---
+++ ejercicio09.py
@@ -0,0 +1,12 @@
+#ejercicio 09
+
+import os
+#declaracion
+
+alumno,promedio="",0.0,
+alumno=os.sys.argv[1]
+promedio=float(os.sys.argv[2])
+#pressecing
+if (promedio>10.5):
+    print(alumno,"aprobado")
+#fin_if
