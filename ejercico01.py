2f5a77a4901bfa7280953a2ccb6bfb8d5d262642
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 15:52:32 -0500

    se agrego el 1er ejercicio

---
+++ ejercico01.py
@@ -0,0 +1,13 @@
+#ejercicio 01
+
+import os
+#declaracion
+
+nombre,puntaje_de_ingreso="",0
+nombre=os.sys.argv[1]
+puntaje_de_ingreso=int(os.sys.argv[2])
+#pressecing
+if (puntaje_de_ingreso>100):
+    print(nombre,"ingreso")
+
+#fin_if
