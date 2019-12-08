5c3ee2522dc1a02198b1935ad229c1a28ca99103
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:03:42 -0500

    se agrego el 3er ejercicio

---
+++ ejercicio03.py
@@ -0,0 +1,13 @@
+#ejercicio 03
+
+import os
+#declaracion
+
+cliente,total_compras="",0.0
+cliente=os.sys.argv[1]
+total_compras=float(os.sys.argv[2])
+#pressecing
+if (total_compras>70):
+    print(cliente,"gano canasta")
+
+#fin_if
