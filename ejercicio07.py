dff0edbdf35d7cc8da626835b26b2acd89bac134
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:37:41 -0500

    se agrego el 7mo ejercicio

---
+++ ejercicio07.py
@@ -0,0 +1,15 @@
+#ejercicio 07
+
+import os
+#declaracion
+
+nombre,edad="",0.0
+nombre=os.sys.argv[1]
+edad=float(os.sys.argv[2])
+#pressecing
+#si la edad es mayor o igual a 65 años
+#mostrar apto para pension
+if (edad>=65):
+    print(nombre,"apto para pension")
+
+#fin_if
