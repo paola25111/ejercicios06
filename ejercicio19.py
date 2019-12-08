486504aba13d5c7566bc3ab28918b8971fc673ab
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 17:01:36 -0500

    se agrego el 19vo ejercicio

---
+++ ejercicio19.py
@@ -0,0 +1,14 @@
+#ejercicio 19
+
+import os
+#declaracion
+
+alumno,promedio="",0.0,
+alumno=os.sys.argv[1]
+promedio=float(os.sys.argv[2])
+#pressecing
+if (promedio>10.5):
+    print(alumno,"aprobado")
+else:
+    print(alumno,"desaprobado")
+#fin_if
