462e5a905e9446e0067e40b814c7e0fd77eb405e
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-08 16:02:50 -0500

    se agrego el 2do ejercicio

---
+++ ejercico02.py
@@ -0,0 +1,13 @@
+#ejercicio 02
+
+import os
+#declaracion
+
+alumno,curso_aprobado="",0
+alumno=os.sys.argv[1]
+curso_aprobado=int(os.sys.argv[2])
+#pressecing
+if (curso_aprobado>=11):
+    print(alumno,"aprobo")
+
+#fin_if
