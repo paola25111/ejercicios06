0f5811a9e80dde8854818a5eb8d84e268ce47ff6
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-10 12:22:21 -0500

    se agrego el lote bat

---
+++ lote.bat
@@ -0,0 +1,75 @@
+python ejercicio06[caceres]/ejercicio01.py "juan" 105
+python ejercicio06[caceres]/ejercicio01.py "pedro" 98
+python ejercicio06[caceres]/ejercicio01.py "paola" 200
+python ejercicio06[caceres]/ejercicio02.py  "carlos" "logica" 12
+python ejercicio06[caceres]/ejercicio02.py  "miguel" "fisica"  11
+python ejercicio06[caceres]/ejercicio02.py  "carla"  "modelos"  10
+python ejercicio06[caceres]/ejercicio03.py  "ricardo" 79
+python ejercicio06[caceres]/ejercicio03.py  "rosa" 30
+python ejercicio06[caceres]/ejercicio03.py  "luis" 90
+python ejercicio06[caceres]/ejercicio04.py  "alexis" 34
+python ejercicio06[caceres]/ejercicio04.py  "alexis" 14
+python ejercicio06[caceres]/ejercicio04.py  "karen" 50
+python ejercicio06[caceres]/ejercicio05.py  "juan" 34
+python ejercicio06[caceres]/ejercicio05.py  "juana" 25
+python ejercicio06[caceres]/ejercicio05.py  "doris" 14
+python ejercicio06[caceres]/ejercicio06.py  "luis" 17
+python ejercicio06[caceres]/ejercicio06.py  "kiara" 19
+python ejercicio06[caceres]/ejercicio06.py  "victor" 23
+python ejercicio06[caceres]/ejercicio07.py  "luis" 55
+python ejercicio06[caceres]/ejercicio07.py  "kiara" 68
+python ejercicio06[caceres]/ejercicio07.py  "victor" 78
+python ejercicio06[caceres]/ejercicio08.py  "edit" 55
+python ejercicio06[caceres]/ejercicio08.py  "sandra" 79
+python ejercicio06[caceres]/ejercicio08.py  "victoria" 23
+python ejercicio06[caceres]/ejercicio09.py  "karen" 55
+python ejercicio06[caceres]/ejercicio09.py  "pamela" 68
+python ejercicio06[caceres]/ejercicio09.py  "rosa" 43
+python ejercicio06[caceres]/ejercicio10.py  "edit" 18
+python ejercicio06[caceres]/ejercicio10.py  "sandra" 19
+python ejercicio06[caceres]/ejercicio10.py  "victoria" 10
+python ejercicio06[caceres]/ejercicio11.py "juan" 105
+python ejercicio06[caceres]/ejercicio11.py "pedro" 98
+python ejercicio06[caceres]/ejercicio1.py "paola" 200
+python ejercicio06[caceres]/ejercicio12.py  "carlos" "logica" 12
+python ejercicio06[caceres]/ejercicio12.py  "miguel" "fisica"  11
+python ejercicio06[caceres]/ejercicio12.py  "carla"  "modelos"  10
+python ejercicio06[caceres]/ejercicio13.py  "ricardo" 79
+python ejercicio06[caceres]/ejercicio13.py  "rosa" 30
+python ejercicio06[caceres]/ejercicio13.py  "luis" 90
+python ejercicio06[caceres]/ejercicio14.py  "alexis" 34
+python ejercicio06[caceres]/ejercicio14.py  "alexis" 14
+python ejercicio06[caceres]/ejercicio14.py  "karen" 50
+python ejercicio06[caceres]/ejercicio15.py  "juan" 34
+python ejercicio06[caceres]/ejercicio15.py  "juana" 25
+python ejercicio06[caceres]/ejercicio15.py  "doris" 14
+python ejercicio06[caceres]/ejercicio16.py  "luis" 17
+python ejercicio06[caceres]/ejercicio16.py  "kiara" 19
+python ejercicio06[caceres]/ejercicio16.py  "victor" 23
+python ejercicio06[caceres]/ejercicio17.py  "luis" 55
+python ejercicio06[caceres]/ejercicio17.py  "kiara" 68
+python ejercicio06[caceres]/ejercicio17.py  "victor" 78
+python ejercicio06[caceres]/ejercicio18.py  "edit" 55
+python ejercicio06[caceres]/ejercicio18.py  "sandra" 79
+python ejercicio06[caceres]/ejercicio18.py  "victoria" 23
+python ejercicio06[caceres]/ejercicio19.py  "karen" 55
+python ejercicio06[caceres]/ejercicio19.py  "pamela" 68
+python ejercicio06[caceres]/ejercicio19.py  "rosa" 43
+python ejercicio06[caceres]/ejercicio20.py  "edit" 18
+python ejercicio06[caceres]/ejercicio20.py  "sandra" 19
+python ejercicio06[caceres]/ejercicio20.py  "victoria" 10
+python ejercicio06[caceres]/ejercicio21.py   "papa"  16  2 0.2
+python ejercicio06[caceres]/ejercicio21.py   "carne"  8  3 0.1
+python ejercicio06[caceres]/ejercicio21.py   "sal"  6  1   0.05
+python ejercicio06[caceres]/ejercicio22.py  "edit" 11
+python ejercicio06[caceres]/ejercicio22.py  "sandra" 10
+python ejercicio06[caceres]/ejercicio22.py  "victoria" 16
+python ejercicio06[caceres]/ejercicio23.py  "karen" 80
+python ejercicio06[caceres]/ejercicio23.py  "pamela" 23
+python ejercicio06[caceres]/ejercicio23.py  "rosa" 106
+python ejercicio06[caceres]/ejercicio24.py  "edit" 18
+python ejercicio06[caceres]/ejercicio24.py  "sandra" 12
+python ejercicio06[caceres]/ejercicio24.py  "victoria" 10
+python ejercicio06[caceres]/ejercicio25.py   "teresa"  20
+python ejercicio06[caceres]/ejercicio25.py   "carmen"  44
+python ejercicio06[caceres]/ejercicio25.py   "sandra"  58
