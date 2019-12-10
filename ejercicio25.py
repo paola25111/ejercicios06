d43c797b5b434a1b1feac7a0e8086d6f79571697
paola25111 <58642007+paola25111@users.noreply.github.com>
2019-12-10 12:00:58 -0500

    se agrego el 25vo ejercicio

---
+++ ejercicio25.py
@@ -0,0 +1,18 @@
+#ejercicio 25
+import os
+#declaracion
+
+cliente,costo_entreda="",0.0,
+cliente=os.sys.argv[1]
+costo_entreda=float(os.sys.argv[2])
+#pressecing
+#si el costo es mayor a 40
+#mostrar zona vip
+#en caso contrario zona corriente
+if (costo_entreda>40):
+    print(cliente,"zona vip")
+else:
+    print(cliente,"zona corriente")
+#fin_if
+if (costo_entreda>50):
+    print(cliente,"zona super vip")
